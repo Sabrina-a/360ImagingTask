5b9abe6f5aad100a28fad1c85e1031059396f732
Sabrina-a <sabrina.aboelfetooh@gmail.com>
2021-06-11 01:30:42 +0200

    deploying

--- routes/comment.js
+++ routes/comment.js
@@ -1,46 +1,71 @@
-const router = require("express").Router()
-const blog = require('../models/blogSchema')
-router.post('/post/:id', async(req,res)=>{
-    const {id } = req.params;
-    const {comment} = req.body;
-    const _Blog = await blog.findOne({_id : id})
- console.log(req);
-  if(_Blog){
-      _Blog.comments.push({content : comment , commentedAt : new Date().toString()})
+const router = require("express").Router();
+const blog = require("../models/blogSchema");
+const _ = require("lodash");
+router.post("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const { comment } = req.body;
+  const _Blog = await blog.findOne({ _id: id });
 
-      await _Blog.save();
-     res.redirect(`/routes/blog/${id}`)
-        // res.json(_Blog);
+  if (_Blog) {
+    _Blog.comments.push({
+      content: comment,
+      commentedAt: new Date().toString(),
+    });
+
+    await _Blog.save();
+    res.redirect(`/routes/blog/${id}`);
+    // res.json(_Blog);
   }
-
 });
 
-
-router.get('/delete/:id', (req,res)=>{
-    const {id} = req.params ; 
-    blog.deleteOne({_id : id})
-    .then(()=>{
-        console.log("Blog deleted successfully !")
-        res.redirect('/')
+router.get("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  blog
+    .deleteOne({ _id: id })
+    .then(() => {
+      console.log("Blog deleted successfully !");
+      res.redirect("/");
     })
-    .catch((err)=>console.log(err));
-})
+    .catch((err) => console.log(err));
+});
 
-router.get('/edit/:id', async(req,res)=>{
-    const {id} = req.params;
-    const getData = await blog.comments.findOne({_id : id});
-    res.render('../views/partials/editComment',{blog : getData});
-})
+router.get("/routes/comment/edit/:id/:blogId", async (req, res) => {
+  const { id, blogId } = req.params;
 
-router.post('/edit/:id',(req , res)=>{
-    const {id} = req.params;
-    const { content } = req.body;
-    blog.comments.updateOne({_id : id },{title , content})
-    .then(()=>{
-        console.log('Updated Successfully!');
-        console.log(blog.comments);
-        res.redirect('/');
-    })
-    .catch((err)=>console.log(err));
-})
+  const _blog = await blog.findOne({ _id: blogId });
+  let comment = _blog.comments.find((item) => item._id == id);
+
+  console.log("comment", { ...comment, blogId });
+  res.render("../views/partials/editComment", {
+    _id: comment._doc._id,
+    content: comment._doc.content,
+    blogId,
+  });
+});
+
+router.post("/routes/comment/edit/:id/:blogId", async (req, res) => {
+  const { id, blogId } = req.params;
+  const { content } = req.body;
+  const _blog = await blog.findOne({ _id: blogId });
+
+  let commentIndex = _.findIndex(_blog.comments, { id });
+  _blog.comments.splice(commentIndex, 1, {
+    content,
+    commentedAt: new Date().toString(),
+  });
+
+  await _blog.save();
+  res.redirect(`/routes/blog/${blogId}`);
+});
+
+router.get("/routes/comment/delete/:id/:blogId", async (req, res) => {
+  const { id, blogId } = req.params;
+  const _blog = await blog.findOne({ _id: blogId });
+  let commentIndex = _.findIndex(_blog.comments, { id });
+  _blog.comments.splice(commentIndex, 1);
+
+  await _blog.save();
+  res.redirect(`/routes/blog/${blogId}`);
+});
+
 module.exports = router;
